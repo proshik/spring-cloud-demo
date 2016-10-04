ace78c633015c0ad56aeed268df48920c4deda9b
prokhor.krylov <PKrylov@luxoft.com>
2016-10-04 18:55:26 +0300

    change parent dependencies

--- api-service/src/main/java/ru/proshik/spring_cloud_demo/api_service/Application.java
+++ api-service/src/main/java/ru/proshik/spring_cloud_demo/api_service/Application.java
@@ -3,14 +3,15 @@ package ru.proshik.spring_cloud_demo.api_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
- * Created by PKrylov on 04.10.2016.
+ * Created by proshik on 04.10.2016.
  */
 //@EnableOAuth2Client
 @EnableFeignClients
-@EnableResourceServer
-@EnableDiscoveryClient
+//@EnableResourceServer
+//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigurationProperties
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
