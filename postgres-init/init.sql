3657380282f34466abafcd3f3b2b3a1abd506269
proshik <proxoraleksandrovich@gmail.com>
2016-12-11 15:27:33 +0300

    0.1.2  - #11. Update docker-compose files. Create Dockerfile for db init.

---
+++ postgres-init/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE think_clearly_account;
+GRANT ALL PRIVILEGES ON DATABASE think_clearly_account TO postgres;
+CREATE DATABASE think_clearly_auth;
+GRANT ALL PRIVILEGES ON DATABASE think_clearly_auth TO postgres;
+CREATE DATABASE think_clearly_topic;
+GRANT ALL PRIVILEGES ON DATABASE think_clearly_auth TO postgres;
