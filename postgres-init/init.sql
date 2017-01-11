3fd8aa725010a865d277030f8b82acbc6d75564a
proshik <proxoraleksandrovich@gmail.com>
2017-01-12 02:12:55 +0300

    0.1.2
     - update docker-compose and env. variables.

--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -3,4 +3,4 @@ GRANT ALL PRIVILEGES ON DATABASE think_clearly_account TO postgres;
 CREATE DATABASE think_clearly_auth;
 GRANT ALL PRIVILEGES ON DATABASE think_clearly_auth TO postgres;
 CREATE DATABASE think_clearly_topic;
-GRANT ALL PRIVILEGES ON DATABASE think_clearly_auth TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE think_clearly_topic TO postgres;
